cbaf4f77e886a06ecff5a9d32fd2ddbfc123904c
Akul-sada <sadananda.sherigara@gmail.com>
2023-05-20 20:36:01 +0530

    write a code for add patient

--- server/routes/patientsRoutes.js
+++ server/routes/patientsRoutes.js
@@ -4,8 +4,31 @@ const router = express.Router();
 
 
 
-router.get("/",(req,res)=>{
-    console.log("connect")
-});
+router.post("/addpatient",async(req,res)=>{
+    // console.log(req.body);
+    const {name,email,age,mobile,work,add,desc} = req.body;
+    
+    
+    
+    if(!id || !name || !age || !gender){
+        res.status(422).json("plz fill the data");
+    }
+
+    try {
+        
+        const patientName = await users.findOne({email:email});
+        
+
+        if(patientName){
+            res.status(422).json("this is user is already present");
+        }else{
+
+            // Save data to database pending
+        }
+
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
 
 export default router;
